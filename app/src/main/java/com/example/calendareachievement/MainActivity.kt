844b7534227dcee3b4fc29d8266c61a746b05553
zuevasasha78 <zuevasasha78@gmail.com>
2020-02-20 22:45:17 +0300

    добавила информацию о придлжении попробовала анимацию

--- app/src/main/java/com/example/calendareachievement/MainActivity.kt
+++ app/src/main/java/com/example/calendareachievement/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.calendareachievement
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.animation.AnimationUtils
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.add_achievement_category).setOnClickListener { v ->
+            val buttonAnimation = AnimationUtils.loadAnimation(this, R.anim.button)
+            v.startAnimation(buttonAnimation)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.dialog, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.about_item) {
+            showInfo()
+        }
+        return true
+    }
+
+    private fun showInfo() {
+        val dialogTitle = getString(R.string.about_title, BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.about_message)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
     }
 }
